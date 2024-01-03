4ef0ecd5be0af5364021a299cda6beff0ff00d38
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2024-01-03 03:05:02 +0100

    sq

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+for i in range(ord('A'), ord('z') + 1):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')
