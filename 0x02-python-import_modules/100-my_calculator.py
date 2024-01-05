eac133e5c1a14849718c0a8db01719772c85f827
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2024-01-05 01:40:21 +0100

    exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv, exit
     from calculator_1 import add, sub, mul, div
     """Comment."""
     argc = len(argv)
     if argc != 4:
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
-
     ops = {'+': add, '-': sub, '*': mul, '/': div}
-
     if argv[2] in ops:
-        n1, sym, n2 = map(int, [argv[1], argv[2], argv[3]])
+        n1, sym, n2 = int(argv[1]), argv[2], int(argv[3])
         x = ops[sym](n1, n2)
         print('{:d} {} {:d} = {:d}'.format(n1, sym, n2, x))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-
     exit(0)
