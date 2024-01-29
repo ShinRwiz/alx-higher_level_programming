28a7cd2aa71a6bd1fdb0ef56ed4cb59a9dada6c6
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2024-01-29 22:31:10 +0100

    test

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """Is a rectangle."""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
